ecca47a76fa4e444b049cf5de49547c2c9bda59d
Hknots <Hknots99@gmail.com>
2023-07-06 10:42:39 +0200

    add path & fix path value

--- src/main/java/no/fintlabs/model/person/PersonService.java
+++ src/main/java/no/fintlabs/model/person/PersonService.java
@@ -66,8 +66,8 @@ public class PersonService {
             kontaktinformasjon.setMobiltelefonnummer(contract.getElev().getMobilNummer());
         personResource.setKontaktinformasjon(kontaktinformasjon);
 
-        personResource.addLarling(Link.with(LarlingResource.class, "systemid", contract.getElev().getSystemId()));
-        personResource.addSelf(Link.with(Person.class, "fodselsnummer", contract.getElev().getFodselsNummer()));
+        personResource.addLarling(Link.with(LarlingResource.class, "utdanning/larling/larling/systemid", contract.getElev().getSystemId()));
+        personResource.addSelf(Link.with(Person.class, "systemid", contract.getElev().getFodselsNummer()));
 
         return personResource;
     }
