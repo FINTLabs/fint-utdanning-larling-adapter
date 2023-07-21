96b4be6ef4424d51283c683a1c8fce9e842c3055
Hknots <Hknots99@gmail.com>
2023-07-21 12:36:19 +0200

    finally fixed links

--- src/main/java/no/fintlabs/model/person/PersonService.java
+++ src/main/java/no/fintlabs/model/person/PersonService.java
@@ -66,8 +66,8 @@ public class PersonService {
             kontaktinformasjon.setMobiltelefonnummer(contract.getElev().getMobilNummer());
         personResource.setKontaktinformasjon(kontaktinformasjon);
 
-        personResource.addSelf(Link.with(Person.class, "fodselsnummer", contract.getElev().getFodselsNummer()));
         personResource.addLarling(Link.with(LarlingResource.class, "systemid", contract.getElev().getSystemId()));
+        personResource.addSelf(Link.with(Person.class, "fodselsnummer", contract.getElev().getFodselsNummer()));
 
         return personResource;
     }
